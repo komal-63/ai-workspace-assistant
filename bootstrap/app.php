41bbc533dd6f152e12ec4ce27ec6cca4f2e05cc2
manjeet <thebloghub.001@gmail.com>
2026-09-09 21:43:10 +0530

    Trust Railway proxy for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
